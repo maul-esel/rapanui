88b5be60bccb0fd34854ef3155cb5542f22f2083
maul.esel <maul.esel@go4more.de>
2015-11-22 17:28:25 +0100

    Transformation: add ruleConclusionIndex for rules with multiple conclusions

--- src/rapanui.core/src/rapanui/core/Transformation.java
+++ src/rapanui.core/src/rapanui/core/Transformation.java
@@ -14,6 +14,7 @@ public class Transformation {
 	private final Term input;
 	private final Term output;
 	private final Rule rule;
+	private final int ruleConclusionIndex;
 	private final Formula[] premiseMatching;
 
 	/**
@@ -22,19 +23,22 @@ public class Transformation {
 	 * @param input The original term which is transformed
 	 * @param output The result of the transformation
 	 * @param rule The underlying rule which allows the transformation
+	 * @param int The index of the used conclusion among the rule's conclusions
 	 * @param premiseMatching Matches the rule's premises to known facts or premises of the environment
 	 */
-	Transformation(Term input, Term output, Rule rule, Formula[] premiseMatching) {
+	Transformation(Term input, Term output, Rule rule, int ruleConclusionIndex, Formula[] premiseMatching) {
 		// TODO: validate parameters? Or rely on valid input?
 
 		assert input != null;
 		assert output != null;
 		assert rule != null;
+		assert 0 <= ruleConclusionIndex && ruleConclusionIndex < rule.getConclusions().size();
 		assert premiseMatching != null;
 
 		this.input = input;
 		this.output = output;
 		this.rule = rule;
+		this.ruleConclusionIndex = ruleConclusionIndex;
 		this.premiseMatching = premiseMatching;
 	}
 
@@ -48,6 +52,10 @@ public class Transformation {
 
 	public Rule getRule() {
 		return rule;
+	}
+
+	public int getRuleConclusionIndex() {
+		return ruleConclusionIndex;
 	}
 
 	public Formula[] getPremiseMatching() {
