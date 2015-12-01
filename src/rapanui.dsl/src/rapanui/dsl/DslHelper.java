7a5e1f0e4b9e99c9f969bbb9f7ddfacdb04c0e8e
maul.esel <maul.esel@go4more.de>
2015-12-01 17:48:22 +0100

    add helper for formula equality

--- src/rapanui.dsl/src/rapanui/dsl/DslHelper.java
+++ src/rapanui.dsl/src/rapanui/dsl/DslHelper.java
@@ -2,10 +2,14 @@ package rapanui.dsl;
 
 import org.eclipse.emf.ecore.util.EcoreUtil;
 
+import rapanui.dsl.moai.Formula;
 import rapanui.dsl.moai.Term;
 
 public class DslHelper {
 	public static boolean equal(Term left, Term right) {
+		return EcoreUtil.equals(left, right);
+	}
+	public static boolean equal(Formula left, Formula right) {
 		return EcoreUtil.equals(left, right);
 	}
 }
