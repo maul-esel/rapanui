fdb26330f118f43170d9ecd1a43a9326e49e3583
maul.esel <maul.esel@go4more.de>
2015-12-03 21:34:46 +0100

    add DslHelper.serialize(Formula|Term) methods

--- src/rapanui.dsl/src/rapanui/dsl/DslHelper.java
+++ src/rapanui.dsl/src/rapanui/dsl/DslHelper.java
@@ -1,6 +1,8 @@
 package rapanui.dsl;
 
 import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.xtext.resource.SaveOptions;
+import org.eclipse.xtext.serializer.ISerializer;
 
 import rapanui.dsl.moai.Formula;
 import rapanui.dsl.moai.Term;
@@ -9,7 +11,24 @@ public class DslHelper {
 	public static boolean equal(Term left, Term right) {
 		return EcoreUtil.equals(left, right);
 	}
+
 	public static boolean equal(Formula left, Formula right) {
 		return EcoreUtil.equals(left, right);
+	}
+
+	public static String serialize(Formula formula) {
+		return new MoaiStandaloneSetup()
+		.createInjectorAndDoEMFRegistration()
+		.getInstance(ISerializer.class)
+		.serialize(formula)
+		.replaceAll("\\s+", " "); // remove extraneous whitespace // TODO: do this properly in a formatter
+	}
+
+	public static String serialize(Term term) {
+		return new MoaiStandaloneSetup()
+		.createInjectorAndDoEMFRegistration()
+		.getInstance(ISerializer.class)
+		.serialize(term)
+		.replaceAll("\\s+", " "); // remove extraneous whitespace // TODO: do this properly in a formatter
 	}
 }
