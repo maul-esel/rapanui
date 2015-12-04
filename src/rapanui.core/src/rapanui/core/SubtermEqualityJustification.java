4b302fd677433c88e625053ed84589ca841dbaf9
maul.esel <maul.esel@go4more.de>
2015-12-04 17:12:18 +0100

    SubtermEqualityJustification: store both subterms

--- src/rapanui.core/src/rapanui/core/SubtermEqualityJustification.java
+++ src/rapanui.core/src/rapanui/core/SubtermEqualityJustification.java
@@ -4,17 +4,23 @@ import rapanui.dsl.moai.Formula;
 import rapanui.dsl.moai.Term;
 
 public class SubtermEqualityJustification extends Justification {
-	private final Term subTerm;
+	private final Term originalSubTerm;
+	private final Term newSubTerm;
 	private final Justification justification;
 
-	public SubtermEqualityJustification(Formula justifiedFormula, Term subTerm, Justification justification) {
+	public SubtermEqualityJustification(Formula justifiedFormula, Term originalSubTerm, Term newSubTerm, Justification justification) {
 		super(justifiedFormula);
-		this.subTerm = subTerm;
+		this.originalSubTerm = originalSubTerm;
+		this.newSubTerm = newSubTerm;
 		this.justification = justification;
 	}
 
-	public Term getSubTerm() {
-		return subTerm;
+	public Term getOriginalSubTerm() {
+		return originalSubTerm;
+	}
+
+	public Term getNewSubTerm() {
+		return newSubTerm;
 	}
 
 	public Justification getJustification() {
