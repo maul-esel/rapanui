87b5643813044a52bdbb89c5e85ec1c862a53a18
maul.esel <maul.esel@go4more.de>
2015-12-05 13:54:20 +0100

    Serializer: add quotes for definition names

--- src/rapanui.dsl/src/rapanui/dsl/Serializer.java
+++ src/rapanui.dsl/src/rapanui/dsl/Serializer.java
@@ -44,7 +44,7 @@ public class Serializer {
 	}
 
 	protected String serialize(DefinitionReference reference) {
-		return serialize(reference.getTarget()) + " is " + reference.getDefinitionName();
+		return serialize(reference.getTarget()) + " is \"" + reference.getDefinitionName() + "\"";
 	}
 
 	protected String serialize(BinaryOperation operation, boolean isNested) {
