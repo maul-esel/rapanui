c752f7ccac261d150d0266dd6558ec2c6a1fc8b7
maul.esel <maul.esel@go4more.de>
2015-12-05 11:07:05 +0100

    implement custom serializer for formulas and terms
    Xtext ISerializer has some problems with EObjects that are
    not contained in a resource. Also, it produces extraneous
    whitespace and mishandles parentheses sometimes.

--- src/rapanui.dsl/src/rapanui/dsl/DslHelper.java
+++ src/rapanui.dsl/src/rapanui/dsl/DslHelper.java
@@ -1,25 +1,14 @@
 package rapanui.dsl;
 
-import org.eclipse.xtext.resource.SaveOptions;
-import org.eclipse.xtext.serializer.ISerializer;
-
 import rapanui.dsl.moai.Formula;
 import rapanui.dsl.moai.Term;
 
 public class DslHelper {
 	public static String serialize(Formula formula) {
-		return new MoaiStandaloneSetup()
-		.createInjectorAndDoEMFRegistration()
-		.getInstance(ISerializer.class)
-		.serialize(formula)
-		.replaceAll("\\s+", " "); // remove extraneous whitespace // TODO: do this properly in a formatter
+		return Serializer.getInstance().serialize(formula);
 	}
 
 	public static String serialize(Term term) {
-		return new MoaiStandaloneSetup()
-		.createInjectorAndDoEMFRegistration()
-		.getInstance(ISerializer.class)
-		.serialize(term)
-		.replaceAll("\\s+", " "); // remove extraneous whitespace // TODO: do this properly in a formatter
+		return Serializer.getInstance().serialize(term);
 	}
 }
---
+++ src/rapanui.dsl/src/rapanui/dsl/Serializer.java
@@ -0,0 +1,78 @@
+package rapanui.dsl;
+
+import rapanui.dsl.moai.*;
+
+public class Serializer {
+	private static final Serializer instance = new Serializer();
+
+	private Serializer() {}
+
+	public static Serializer getInstance() {
+		return instance;
+	}
+
+	public String serialize(Formula formula) {
+		if (formula instanceof Equation)
+			return serialize((Equation)formula);
+		else if (formula instanceof Inclusion)
+			return serialize((Inclusion)formula);
+		else if (formula instanceof DefinitionReference)
+			return serialize((DefinitionReference)formula);
+		throw new IllegalStateException("Unknown formula type: " + formula.getClass());
+	}
+
+	public String serialize(Term term) {
+		return serialize(term, false);
+	}
+
+	protected String serialize(Term term, boolean isNested) {
+		if (term instanceof BinaryOperation)
+			return serialize((BinaryOperation)term, isNested);
+		else if (term instanceof UnaryOperation)
+			return serialize((UnaryOperation)term, isNested);
+		else if (term instanceof VariableReference)
+			return serialize((VariableReference)term);
+		else if (term instanceof ConstantReference)
+			return serialize((ConstantReference)term);
+		throw new IllegalStateException("Unknown term type: " + term.getClass());
+	}
+
+	protected String serialize(Equation equation) {
+		return serialize(equation.getLeft()) + " = " + serialize(equation.getRight());
+	}
+
+	protected String serialize(Inclusion inclusion) {
+		return serialize(inclusion.getLeft()) + " ⊆ " + serialize(inclusion.getRight());
+	}
+
+	protected String serialize(DefinitionReference reference) {
+		return serialize(reference.getTarget()) + " is " + reference.getDefinitionName();
+	}
+
+	protected String serialize(BinaryOperation operation, boolean isNested) {
+		String serialized = serialize(operation.getLeft(), true)
+				+ " " + operation.getOperator().getLiteral() + " "
+				+ serialize(operation.getRight(), true);
+
+		if (isNested)
+			return "(" + serialized + ")";
+		return serialized;
+	}
+
+	protected String serialize(UnaryOperation operation, boolean isNested) {
+		String serialized = serialize(operation.getOperand(), true)
+				+ operation.getOperator().getLiteral();
+
+		if (isNested)
+			return "(" + serialized + ")";
+		return serialized;
+	}
+
+	protected String serialize(VariableReference variable) {
+		return variable.getVariable();
+	}
+
+	protected String serialize(ConstantReference constant) {
+		return constant.getConstant();
+	}
+}
