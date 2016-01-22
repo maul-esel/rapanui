d92b054bb7d5e8e9707d82d2a064a54c76bd9142
maul.esel <maul.esel@go4more.de>
2016-01-22 20:58:39 +0100

    [DSL] Builder: always copy parameters
    Since each Term instance can only be in at most one Formula or Term,
    re-using instances removes them from the original object they belong
    to. Thus, instead of leaving the responsibility to copy the terms to
    callers, do it in Builder directly. Also, add null checks for the
    arguments.

--- src/rapanui.dsl/src/rapanui/dsl/Builder.java
+++ src/rapanui.dsl/src/rapanui/dsl/Builder.java
@@ -4,41 +4,72 @@ import org.eclipse.emf.ecore.util.EcoreUtil;
 
 public abstract class Builder {
 	public static Equation reverse(Equation input) {
-		return createEquation(EcoreUtil.copy(input.getRight()), EcoreUtil.copy(input.getLeft()));
+		return createEquation(input.getRight(), input.getLeft());
 	}
 
+	/**
+	* NOTE: this method copies the input terms!
+	*/
 	public static Equation createEquation(Term left, Term right) {
+		assert left != null;
+		assert right != null;
+
 		Equation output = DslFactory.eINSTANCE.createEquation();
-		output.setLeft(left);
-		output.setRight(right);
+		output.setLeft(EcoreUtil.copy(left));
+		output.setRight(EcoreUtil.copy(right));
 		return output;
 	}
 
+	/**
+	* NOTE: this method copies the input terms!
+	*/
 	public static Inclusion createInclusion(Term left, Term right) {
+		assert left != null;
+		assert right != null;
+
 		Inclusion output = DslFactory.eINSTANCE.createInclusion();
-		output.setLeft(left);
-		output.setRight(right);
+		output.setLeft(EcoreUtil.copy(left));
+		output.setRight(EcoreUtil.copy(right));
 		return output;
 	}
 
+	/**
+	* NOTE: this method copies the input term!
+	*/
 	public static DefinitionReference createDefinitionReference(Term target, Definition definition) {
+		assert target != null;
+		assert definition != null;
+
 		DefinitionReference reference = DslFactory.eINSTANCE.createDefinitionReference();
-		reference.setTarget(target);
+		reference.setTarget(EcoreUtil.copy(target));
 		reference.setDefinition(definition);
 		return reference;
 	}
 
+	/**
+	* NOTE: this method copies the input terms!
+	*/
 	public static BinaryOperation createBinaryOperation(Term left, BINARY_OPERATOR operator, Term right) {
+		assert left != null;
+		assert operator != null;
+		assert right != null;
+
 		BinaryOperation output = DslFactory.eINSTANCE.createBinaryOperation();
-		output.setLeft(left);
+		output.setLeft(EcoreUtil.copy(left));
 		output.setOperator(operator);
-		output.setRight(right);
+		output.setRight(EcoreUtil.copy(right));
 		return output;
 	}
 
+	/**
+	* NOTE: this method copies the input term!
+	*/
 	public static UnaryOperation createUnaryOperation(Term operand, POSTFIX_UNARY_OPERATOR operator) {
+		assert operand != null;
+		assert operator != null;
+
 		UnaryOperation output = DslFactory.eINSTANCE.createUnaryOperation();
-		output.setOperand(operand);
+		output.setOperand(EcoreUtil.copy(operand));
 		output.setOperator(operator);
 		return output;
 	}
