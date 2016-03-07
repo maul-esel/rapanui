a2f1ff72c6c1e44c4e9dafdb5de67a9e8de32cbd
maul.esel <maul.esel@go4more.de>
2016-03-07 21:07:10 +0100

    Use java's LinkedHashMap instead of custom Cache class

--- src/rapanui.ui/src/rapanui/ui/views/SuggestionListCellRenderer.java
+++ src/rapanui.ui/src/rapanui/ui/views/SuggestionListCellRenderer.java
@@ -1,22 +1,30 @@
 package rapanui.ui.views;
 
 import java.awt.Component;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import javax.swing.JList;
 import javax.swing.ListCellRenderer;
 
 import rapanui.core.Transformation;
 
-import rapanui.ui.Cache;
-
 public class SuggestionListCellRenderer implements ListCellRenderer<Transformation> {
-	private static final int MAX_VIEW_CACHE_SIZE = 40;
-	private final Cache<Transformation, SuggestionView> viewCache = new Cache<Transformation, SuggestionView>(MAX_VIEW_CACHE_SIZE);
+	private static final int MAX_VIEW_CACHE_SIZE = 60;
+	private static final int INITIAL_VIEW_CACHE_SIZE = 10;
+
+	@SuppressWarnings("serial")
+	private final LinkedHashMap<Transformation, SuggestionView> viewCache
+		= new LinkedHashMap<Transformation, SuggestionView>(INITIAL_VIEW_CACHE_SIZE, 0.75f, true) {
+		@Override protected boolean removeEldestEntry(Map.Entry<Transformation, SuggestionView> eldest) {
+			return size() > MAX_VIEW_CACHE_SIZE;
+		}
+	};
 
 	@Override
 	public Component getListCellRendererComponent(JList<? extends Transformation> list, Transformation suggestion, int index,
 			boolean cellIsSelected, boolean cellHasFocus) {
-		SuggestionView view = viewCache.get(suggestion, sugg -> new SuggestionView(sugg));
+		SuggestionView view = viewCache.computeIfAbsent(suggestion, sugg -> new SuggestionView(sugg));
 		view.setSelected(cellIsSelected);
 		return view;
 	}
