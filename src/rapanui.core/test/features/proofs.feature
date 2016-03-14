9ddb3aeef490137ccfebae81a92c814cb6df0c49
maul.esel <maul.esel@go4more.de>
2016-03-14 18:26:46 +0100

    Run some proofs as cucumber tests

---
+++ src/rapanui.core/test/features/proofs.feature
@@ -0,0 +1,73 @@
+Feature: Simple proofs
+
+  Specify several simple proofs as cucumber scenarios
+
+  Scenario: Prove R = R˘ for symmetric relations
+    Given I have a proof environment with the rule system "symmetric_equality.raps"
+    And I add the premise that R is symmetric
+
+    When I start a new conclusion with start term R
+    Then the equal term R˘˘ is suggested
+    When I apply this suggestion
+    Then the greater term R˘ is suggested
+
+    When I apply this suggestion
+    And I start a new conclusion with start term R
+    Then the equal term R˘ is suggested
+
+  Scenario: Prove that the converse of a linear order is also a linear order
+    Given I have a proof environment with the rule system "converse_order.raps"
+    And I add the premise that R is a partial order
+    And I add the premise that R is linear
+
+    # Reflexivity:
+
+    When I start a new conclusion with start term I
+    Then the equal term I˘ is suggested
+    When I apply this suggestion
+    Then the greater term R˘ is suggested
+
+    # Transitivity:
+
+    When I start a new conclusion with start term R˘;R˘
+    Then the equal term (R;R)˘ is suggested
+    When I apply this suggestion
+    Then the greater term R˘ is suggested
+
+    # Antisymmetry:
+
+    When I start a new conclusion with start term R˘ ∩ R˘˘
+    Then the equal term R˘ ∩ R is suggested
+    When I apply this suggestion
+    Then the equal term R ∩ R˘ is suggested
+    When I apply this suggestion
+    Then the greater term I is suggested
+
+    # Linearity:
+
+    When I start a new conclusion with start term R˘ ∪ R˘˘
+    Then the equal term R˘ ∪ R is suggested
+    When I apply this suggestion
+    Then the equal term R ∪ R˘ is suggested
+    When I apply this suggestion
+    Then the equal term Π is suggested
+
+  Scenario: Proof R = I if R is an equivalence relation and a partial order
+    Given I have a proof environment with the rule system "equivalence_order.raps"
+    And I add the premise that R is an equivalence relation
+    And I add the premise that R is a partial order
+
+    When I start a new conclusion with start term R
+    Then the equal term R˘˘ is suggested
+    When I apply this suggestion
+    Then the greater term R˘ is suggested
+
+    When I apply this suggestion
+    And I start a new conclusion with start term R
+    Then the equal term R ∩ R˘ is suggested
+    When I apply this suggestion
+    Then the greater term I is suggested
+
+    When I apply this suggestion
+    And I start a new conclusion with start term R
+    Then the equal term I is suggested
---
+++ src/rapanui.core/test/rapanui/core/test/ProofSteps.java
@@ -0,0 +1,78 @@
+package rapanui.core.test;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+import com.google.common.base.Objects;
+
+import cucumber.api.java.en.*;
+import rapanui.core.*;
+import rapanui.dsl.*;
+
+public class ProofSteps {
+	private static final long SUGGESTION_TIMEOUT = 100;
+
+	private ProofEnvironment currentEnvironment;
+	private ConclusionProcess currentConclusion;
+	private Transformation currentSuggestion;
+
+	@Given("I have a proof environment with the rule system \"([^\"]+)\"")
+	public void createProofEnvironent(String ruleSystem) {
+		RuleSystemCollection ruleSystems = new RuleSystemCollection();
+		ruleSystems.load("test/libraries/" + ruleSystem);
+		currentEnvironment = new ProofEnvironment(ruleSystems);
+	}
+
+	@Given("I add the premise that (.*) is (.*)")
+	public void addPremise(String termSyntax, String definitionName) {
+		Term term = Parser.getInstance().parseTerm(termSyntax);
+		Definition definition = currentEnvironment.getRuleSystems().resolveDefinition(definitionName);
+		DefinitionReference reference = Builder.createDefinitionReference(term, definition);
+		currentEnvironment.addPremise(reference);
+	}
+
+	@Given("I add the formula (.*) as premise")
+	public void addPremise(String formulaSyntax) {
+		Formula premise = Parser.getInstance().parseFormula(formulaSyntax);
+		currentEnvironment.addPremise(premise);
+	}
+
+	@When("I start a new conclusion with start term (.*)")
+	public void startConclusion(String termSyntax) {
+		Term term = Parser.getInstance().parseTerm(termSyntax);
+		currentConclusion = currentEnvironment.addConclusion(term);
+	}
+
+	@Then("the (greater|equal)? term (.*) is suggested")
+	public void findSuggestion(String greaterOrEqual, String termSyntax) {
+		final Term term = Parser.getInstance().parseTerm(termSyntax);
+
+		BINARY_RELATION suggestionType = BINARY_RELATION.UNSPECIFIED;
+		if (Objects.equal(greaterOrEqual, "equal"))
+			suggestionType = BINARY_RELATION.EQUATION;
+		else if (Objects.equal(greaterOrEqual, "greater"))
+			suggestionType = BINARY_RELATION.INCLUSION;
+
+		List<Transformation> suggestions = Collections.synchronizedList(new LinkedList<Transformation>());
+		SuggestionFinder.getDefaultInstance().makeSuggestionsAsync(currentConclusion, suggestionType)
+			.onEmit(suggestions::add);
+
+		try {
+			Thread.sleep(SUGGESTION_TIMEOUT);
+		} catch (InterruptedException e) {}
+
+		Optional<Transformation> requiredSuggestion = suggestions.stream()
+			.filter(suggestion -> suggestion.getOutput().structurallyEquals(term))
+			.findAny();
+		assert requiredSuggestion.isPresent() : "The term " + termSyntax + " was not suggested.";
+
+		currentSuggestion = requiredSuggestion.get();
+	}
+
+	@When("I apply this suggestion")
+	public void applySuggestion() {
+		currentConclusion.appendTransformation(currentSuggestion);
+	}
+}
