5e3a140866b460701a1c3f21792e26b2c5f839cd
maul.esel <maul.esel@go4more.de>
2016-04-08 00:53:25 +0200

    Serializer: remove pseudo-singleton pattern, make class "static"

--- src/rapanui.dsl/src/rapanui/dsl/Serializer.java
+++ src/rapanui.dsl/src/rapanui/dsl/Serializer.java
@@ -3,93 +3,86 @@ package rapanui.dsl;
 import java.util.Stack;
 
 public class Serializer implements Visitor {
-	private static final ThreadLocal<Serializer> instances = new ThreadLocal<Serializer>() {
-		@Override protected Serializer initialValue() {
-			return new Serializer();
-		}
-	};
+	private Serializer() {} // static class
 
-	private final Stack<String> result = new Stack<String>();
-
-	private Serializer() {}
-
-	public static Serializer getInstance() {
-		return instances.get();
+	public static String serialize(Rule rule) {
+		SerializationVisitor visitor = new SerializationVisitor();
+		rule.accept(visitor);
+		return visitor.getResult();
 	}
 
-	public String serialize(Rule rule) {
-		result.clear();
-		rule.accept(this);
-
-		assert result.size() == 1;
-		return result.pop();
+	public static String serialize(Predicate predicate) {
+		SerializationVisitor visitor = new SerializationVisitor();
+		predicate.accept(visitor);
+		return visitor.getResult();
 	}
 
-	public String serialize(Predicate predicate) {
-		result.clear();
-		predicate.accept(this);
-
-		assert result.size() == 1;
-		return result.pop();
-	}
-
-	public String serialize(Term term) {
+	public static String serialize(Term term) {
 		assert term.isComplete();
-		result.clear();
-		term.accept(this);
 
-		assert result.size() == 1;
-		return result.pop();
+		SerializationVisitor visitor = new SerializationVisitor();
+		term.accept(visitor);
+		return visitor.getResult();
 	}
 
-	@Override public void visit(Rule rule) {
-		String conclusions = result.pop();
-		for (int i = 1; i < rule.getConclusions().size(); ++i)
-			conclusions = result.pop() + "\n\tand " + conclusions;
+	private static class SerializationVisitor implements Visitor {
+		private final Stack<String> result = new Stack<String>();
 
-		if (rule.getPremises().size() == 0)
-			result.push("axiom \"" + rule.getName() + "\"\n\talways " + conclusions + "\n");
-		else {
-			String premises = result.pop();
-			for (int i = 1; i < rule.getPremises().size(); ++i)
-				premises = result.pop() + "\n\tand " + premises;
-			result.push("theorem \"" + rule.getName() + "\"\n\tif " + premises + "\n\tthen " + conclusions + "\n");
+		String getResult() {
+			assert result.size() == 1;
+			return result.pop();
 		}
-	}
 
-	@Override public void visit(Formula formula) {
-		String right = result.pop(), left = result.pop();
-		result.push(left + " " + formula.getFormulaType().getLiteral() + " " + right);
-	}
+		@Override public void visit(Rule rule) {
+			String conclusions = result.pop();
+			for (int i = 1; i < rule.getConclusions().size(); ++i)
+				conclusions = result.pop() + "\n\tand " + conclusions;
 
-	@Override public void visit(DefinitionReference reference) {
-		result.push(result.pop() + " is \"" + reference.getDefinition().getName() + "\"");
-	}
+			if (rule.getPremises().size() == 0)
+				result.push("axiom \"" + rule.getName() + "\"\n\talways " + conclusions + "\n");
+			else {
+				String premises = result.pop();
+				for (int i = 1; i < rule.getPremises().size(); ++i)
+					premises = result.pop() + "\n\tand " + premises;
+				result.push("theorem \"" + rule.getName() + "\"\n\tif " + premises + "\n\tthen " + conclusions + "\n");
+			}
+		}
 
-	@Override public void visit(BinaryOperation operation) {
-		String right = result.pop(), left = result.pop();
+		@Override public void visit(Formula formula) {
+			String right = result.pop(), left = result.pop();
+			result.push(left + " " + formula.getFormulaType().getLiteral() + " " + right);
+		}
 
-		if (operation.getLeft() instanceof BinaryOperation)
-			left = "(" + left + ")";
-		if (operation.getRight() instanceof BinaryOperation)
-			right = "(" + right + ")";
+		@Override public void visit(DefinitionReference reference) {
+			result.push(result.pop() + " is \"" + reference.getDefinition().getName() + "\"");
+		}
 
-		result.push(left + " " + operation.getOperator().getLiteral() + " " + right);
-	}
+		@Override public void visit(BinaryOperation operation) {
+			String right = result.pop(), left = result.pop();
 
-	@Override public void visit(UnaryOperation operation) {
-		String operand = result.pop();
+			if (operation.getLeft() instanceof BinaryOperation)
+				left = "(" + left + ")";
+			if (operation.getRight() instanceof BinaryOperation)
+				right = "(" + right + ")";
 
-		if (operation.getOperand() instanceof BinaryOperation || operation.getOperand() instanceof UnaryOperation)
-			operand = "(" + operand + ")";
+			result.push(left + " " + operation.getOperator().getLiteral() + " " + right);
+		}
 
-		result.push(operand + operation.getOperator().getLiteral());	}
+		@Override public void visit(UnaryOperation operation) {
+			String operand = result.pop();
 
-	@Override public void visit(VariableReference variable) {
-		result.push(variable.getVariable());
-	}
+			if (operation.getOperand() instanceof BinaryOperation || operation.getOperand() instanceof UnaryOperation)
+				operand = "(" + operand + ")";
 
-	@Override public void visit(ConstantReference constant) {
-		result.push(constant.getConstant());
+			result.push(operand + operation.getOperator().getLiteral());
+		}
+
+		@Override public void visit(VariableReference variable) {
+			result.push(variable.getVariable());
+		}
+
+		@Override public void visit(ConstantReference constant) {
+			result.push(constant.getConstant());
+		}
 	}
 }
